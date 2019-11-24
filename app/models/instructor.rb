fc64384fedd9a715ad8a8753ce6c97eb358c8255
Miss-Cheese <nat.galligan@gmail.com>
2019-11-24 14:45:27 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,47 @@
 class Instructor
+    attr_reader :name
+   
+    
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def find_student_test(student, test_name)
+        student.student_tests.find {|test|
+        test.test_name == test_name
+        }
+    end
+
+    def pass_student(student, test_name)
+        result = find_student_test(student, test_name)
+
+        if result
+            result.test_status = "passed"
+        else
+            result = BoatingTest.new(student, test_name, "passed", self)
+        end
+        result
+    end
+
+    
+    def fail_student(student, test_name)
+        result = find_student_test(student, test_name)
+
+        if result
+            result.test_status = "failed"
+        else
+            result = BoatingTest.new(student, test_name, "failed", self)
+        end
+        result
+    end
+
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,49 @@
 class Student
+    attr_reader :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_student(name)
+        @@all.find {|student|
+        student.first_name == name
+        }
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def student_tests
+        BoatingTest.all.select {|test|
+        test.student == self
+        }
+    end
+
+    def number_of_student_tests
+        student_tests.length
+    end
+
+    def passed_tests
+        student_tests.select {|test|
+        test.test_status == "passed"
+        }
+    end
+
+    def number_of_passed_tests
+        passed_tests.length
+    end
+
+    def grade_percentage
+        (number_of_passed_tests/number_of_student_tests.to_f) * 100
+    end
 
 end
